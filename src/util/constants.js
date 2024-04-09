d8ab04589e59ec1d39aa629481fa37b3d0c644d5
Joshua Graber <joshua.d.graber@gmail.com>
2024-04-09 12:23:44 -0400

    refactor(util): remove constants file

--- src/util/constants.js
+++
@@ -1,7 +0,0 @@
-export const ENDPOINTS = {
-	IPIFY: 'https://api.ipify.org?format=json',
-	GEOCODE_FORWARD: 'http://api.positionstack.com/v1/forward',
-	GEOCODE_REVERSE: 'http://api.positionstack.com/v1/reverse',
-	PDAP_DATA_SOURCE_SEARCH:
-		'https://data-sources.pdap.io/api/search-tokens?endpoint=quick-search&arg1=all&arg2=all',
-};
